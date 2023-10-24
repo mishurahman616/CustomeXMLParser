557ae4e88732fb93997a020042db8d598d63f35c
Mishu Rahman <mishu.cse616@gmail.com>
2023-10-25 02:04:19 +0600

    completed

---
+++ XMLParser/Program.cs
@@ -0,0 +1,133 @@
+﻿using Assignment2;
+using System.Text;
+using System;
+
+Course course = new Course();
+
+Address presentAddress = new Address
+{
+    Street = "184, Begum Rokeya Sarani",
+    City = "Dhaka",
+    Country = "Bangladesh"
+};
+Address permanantAddress = new Address
+{
+    Street = "184, Begum Rokeya Sarani",
+    City = "Dhaka",
+    Country = "Bangladesh"
+
+};
+Phone phone1 = new Phone()
+{
+    Number = "1794914570",
+    Extension = "111",
+    CountryCode = "880"
+};
+Phone phone2 = new Phone()
+{
+    Number = "1794914571",
+    Extension = "111",
+    CountryCode = "880"
+};
+List<Phone> phoneNumbers = new List<Phone> { phone1, phone2 };
+Instructor teacher = new Instructor()
+{
+    Name = "Jalaluddin",
+    Email = "jalaluddin@gmail.com",
+    PresentAddress = presentAddress,
+    PermanentAddress = permanantAddress,
+    PhoneNumbers = phoneNumbers,
+};
+
+Session session1 = new Session()
+{
+    DurationInHour = 2,
+    LearningObjective = "How Delegates works!"
+};
+Session session2 = new Session()
+{
+    DurationInHour = 2,
+    LearningObjective = "How Delegates works!"
+};
+
+Session session3 = new Session()
+{
+    DurationInHour = 2,
+    LearningObjective = "How Action, Func and Event works!"
+};
+List<Session> topic1Sessions = new List<Session>();
+topic1Sessions.Add(session1);
+topic1Sessions.Add(session2);
+
+List<Session> topic2Sessions = new List<Session>();
+topic2Sessions.Add(session3);
+
+Topic topic1 = new Topic()
+{
+    Title = "Delegates",
+    Description = "This is a demo text",
+    Sessions = topic1Sessions,
+};
+Topic topic2 = new Topic()
+{
+    Title = "Delegates",
+    Description = "This is a demo text",
+    Sessions = topic2Sessions,
+};
+
+List<Topic> topics = new List<Topic>();
+topics.Add(topic1);
+topics.Add(topic2);
+
+AdmissionTest admissionTest1 = new AdmissionTest()
+{
+    StartDateTime = DateTime.Parse("10:00AM"),
+    EndDateTime = DateTime.Parse("01/01/2023 12:00AM"),
+    TestFees = 500,
+};
+
+List<AdmissionTest> admissionTests = new List<AdmissionTest>();
+admissionTests.Add(admissionTest1);
+
+
+course.Title = "Asp.net";
+course.Fees = 30000;
+course.Teacher = teacher;
+course.Topics = topics;
+course.Tests = admissionTests;
+
+
+Dictionary<string, int> dic = new Dictionary<string, int>();
+dic.Add("one", 1);
+dic.Add("two", 2);
+dic.Add("three", 3);
+
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+    
+List<object> list2 = new List<object>();
+list2.Add(list);
+list2.Add(new Course());
+list2.Add(new Course());
+
+List<double> list3 = new List<double>();
+list3.Add(1);
+list3.Add(2);
+
+Dictionary<int, object> dic3 = new Dictionary<int, object>();
+dic3.Add(1, new Course());
+dic3.Add(2, 2);
+dic3.Add(3, new Instructor());
+
+Dictionary<string, object> dic2 = new Dictionary<string, object>();
+dic2.Add("one", new Course());
+dic2.Add("two", 2);
+dic2.Add("three", new Instructor());
+
+char[] str = new char[] {'a', 'b', 'c' };
+
+
+
+Console.WriteLine(XMLFormatter.Convert(course)); 
---
+++ XMLParser/XMLFormatter.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Assignment2
+{
+    public class XMLFormatter
+    {
+        public static StringBuilder XMLConverter(object o, int indent, bool firstCall=false)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder indentString = new StringBuilder(new string(' ', indent));
+            if (o == null)
+            {
+                return null;
+            }
+            if (o is DateTime)
+            {
+                DateTime datetime = (DateTime)o;
+                sb.Append($"{indentString}" + datetime.ToString());
+            }
+            else if (o is Array || o is IList)
+            {
+                string type = o.GetType().ToString();
+                string typeName = type.Substring(type.LastIndexOf('.') + 1).Trim(new char[] { '[', ']' });
+
+                sb.AppendLine($"{indentString}<ArrayOf{typeName}>");
+                foreach (var item in o as IList)
+                {
+                    sb.AppendLine($"{indentString}<{typeName}>");
+                    sb.AppendLine(XMLConverter(item, indent + 2).ToString());
+                    sb.AppendLine($"{indentString}</{typeName}>");
+                }
+                sb.Append($"{indentString}</ArrayOf{typeName}>");
+            } 
+            else if (o is IDictionary)
+            {
+                
+                sb.AppendLine($"{indentString}<Dictionary>");
+                var dictionary= o as IDictionary;
+
+                if(dictionary != null )
+                {
+                    foreach ( var key in dictionary.Keys )
+                    {
+                        var numberkey="";
+                        if (IsNumber(key))
+                        {
+                            numberkey = "key" + key.ToString();
+                        }
+                        else
+                        {
+                            numberkey = key.ToString();
+                        }
+                        sb.AppendLine($"{indentString}<{numberkey}>");
+                        sb.AppendLine(XMLConverter(dictionary[key], indent+2).ToString());
+                        sb.AppendLine($"{indentString}</{numberkey}>");
+
+                    }
+                }
+                sb.AppendLine($"{indentString}</Dictionary>");
+
+            } 
+            else if (IsPreemtive(o))
+            {
+                sb.Append($"{indentString}{o}");
+            }
+            else
+            {
+                var name = o.GetType().Name;
+                var properties = o.GetType().GetProperties();
+                if (firstCall)
+                {
+
+                    //sb.AppendLine($"<?xml version=\"1.0\" encoding=\"Codepage-437\"?>");
+                    //sb.AppendLine($"{indentString}<{name} xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">");
+                    sb.AppendLine($"{indentString}<{name}>");
+                }
+               
+                foreach (var property in properties)
+                {
+                    var propValue = property.GetValue(o, null);
+                    var list = propValue as IList;
+                    var dict = propValue as IDictionary;   
+                    if (list != null)
+                    {
+                        sb.AppendLine($"  {indentString}<{property.Name}>");
+                        foreach (var item in list)
+                        {
+                            sb.AppendLine($"    {indentString}<{item.GetType().Name}>");
+                            sb.AppendLine(XMLConverter(item, indent + 4).ToString());
+                            sb.AppendLine($"    {indentString}</{item.GetType().Name}>");
+                        }
+                        sb.AppendLine($"  {indentString}</{property.Name}>");
+                    }else if(dict is IDictionary)
+                    {
+
+                        sb.AppendLine($"  {indentString}<{property.Name}>");
+                        if (dict != null)
+                        {
+                            foreach (var key in dict.Keys)
+                            {
+                                var numberkey = "";
+                                if (IsNumber(key))
+                                {
+                                    numberkey = "key" + key.ToString();
+                                }
+                                else
+                                {
+                                    numberkey = key.ToString();
+                                }
+                                sb.AppendLine($"    {indentString}<{numberkey}>");
+                                sb.AppendLine(XMLConverter(dict[key], indent + 6).ToString());
+                                sb.AppendLine($"    {indentString}</{numberkey}>");
+
+                            }
+                        }
+                        sb.AppendLine($"  {indentString}</{property.Name}>");
+                    }
+
+                    else
+                    {
+                        sb.AppendLine($"  {indentString}<{property.Name}>");
+                        if (propValue != null)
+                        {
+                            sb.AppendLine(XMLConverter(propValue, indent + 4).ToString());
+                        }
+
+                        sb.AppendLine($"  {indentString}</{property.Name}>");
+                    }
+
+                }
+                if (firstCall)
+                {
+                    sb.Append($"{indentString}</{name}>");
+                }
+                
+            }
+
+            return sb;
+        }
+        public static bool IsPreemtive(object o)
+        {
+            return IsNumber(o)
+                || o is string
+                || o is StringBuilder
+                || o is char;
+        }
+        public static bool IsNumber(object o)
+        {
+            return o is sbyte
+                || o is byte
+                || o is short
+                || o is ushort
+                || o is int
+                || o is uint
+                || o is long
+                || o is ulong
+                || o is float
+                || o is double
+                || o is decimal
+                || o is Single;
+
+        }
+        public static string Convert(object o)
+        {
+            //XmlSerializer xs = new XmlSerializer(o.GetType());
+            //xs.Serialize(Console.Out, o);
+            StringBuilder sb = new StringBuilder();
+            if (o == null) return null;
+            if (IsPreemtive(o))
+            {
+                sb.Append($"<{o.GetType().Name}>");
+                sb.Append($"{o}");
+                sb.Append($"</{o.GetType().Name}>");
+                return sb.ToString();
+            }else if(o is DateTime)
+            {
+                sb.Append($"<{o.GetType().Name}>");
+                sb.Append((DateTime)o);
+                sb.Append($"</{o.GetType().Name}>");
+                return sb.ToString();
+            }
+
+            var xml= XMLConverter(o, 0, true);
+            
+            return xml.ToString();
+        }
+    }
+}
